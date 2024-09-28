5a1052912918ff8ad6f35c74d3104a42750fe4cb
Brent Scriver <bscriver@oneoddsock.com>
2024-09-28 01:54:57 -0400

    Fix missing %

--- BG3Extender/PostBuild.cmd
+++ BG3Extender/PostBuild.cmd
@@ -11,7 +11,7 @@ IF "%SRC_FILE%"=="" (
 	echo The source binary file was not specified to copy to the Baldur's Gate 3 bin folder.
 	exit /b 2
 )
-SET BG3_BIN_FOLDER_CONFIG_FILE="LOCALAPPDATA%\BG3 Script Extender\BG3Folder.txt"
+SET BG3_BIN_FOLDER_CONFIG_FILE="%LOCALAPPDATA%\BG3 Script Extender\BG3Folder.txt"
 SET BG3_FOLDER=D:\SteamLibrary\steamapps\common\Baldurs Gate 3
 IF EXIST %BG3_BIN_FOLDER_CONFIG_FILE% SET /P BG3_FOLDER=<%BG3_BIN_FOLDER_CONFIG_FILE%
 SET BG3_TARGET_FILE=%BG3_FOLDER%\bin\DWrite.dll
